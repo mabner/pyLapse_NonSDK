1c9c7df977a3e2dc4973eb12b485f8557a64b496
Marcos Leite <m.abner@live.co.uk>
2024-02-26 01:17:03 -0300

    basic commands and config

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from sh import gphoto2 as gp
 import signal, os, subprocess
 
+
 # kills the gphoto2 process opened by gvfs
 def killgPhoto2Process():
     p = subprocess.Popen(['ps', '-A'], stdout=subprocess.PIPE)
@@ -13,4 +14,16 @@ def killgPhoto2Process():
         if b'gvfsd-gphoto2' in line:
             pid = int(line.split(None, 1)[0])
             os.kill(pid, signal.SIGKILL)
+
+
+session_name = 'pyLapse_Test'
+
+shot_date = datetime.now().strftime('%Y%m%d')
+shot_time = datetime.now().strftime('%Y%m%d-%H%M%S')
+
+triggerCommand = ['--trigger-capture']
+downloadCommand = ['--get-all-files']
+
+folder_name = shot_date
+download_location = '/home/leyd/Pictures/Timelapse' + folder_name
 
